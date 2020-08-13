d7517a2d03a892fadd34dca0b12f241c5b80c67a
aplennie <alinapavel@rogers.com>
2020-08-12 20:43:24 -0400

    added work page

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 var tooltipSpan = document.getElementsByClassName('tooltiptext')[0];
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
 
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         tooltipSpan.innerHTML = "hey you! yea you. ya like light mode? click me.";
+        localStorage.setItem('theme', 'dark'); 
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
         tooltipSpan.innerHTML = "hey you! yea you. ya like dark mode? click me.";
+        localStorage.setItem('theme', 'light'); 
     }    
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
 
 //https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 
